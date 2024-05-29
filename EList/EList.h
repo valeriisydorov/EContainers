377e692b0d88c766ee3458065dedbc047cbc644e
Valerii Sydorov <valeriisydorov@gmail.com>
2024-05-29 13:51:26 +0300

    Creating the structure of the List template

--- EList/EList.h
+++ EList/EList.h
@@ -1,4 +1,49 @@
 #ifndef ELIST_H
 #define ELIST_H
 
+
+template <typename T> class EList {
+public:
+    class Iterator {
+    public:
+    private:
+    };
+private:
+    class Node {
+    public:
+    private:
+};
+
+//    ctor (size, value)
+//    dtor
+
+//    copy ctor
+//    copy =
+//    move ctor
+//    move =
+
+//    push_front(value)
+//    push_back(value)
+//    insert(index, value)
+
+//    iterator [] const
+//    iterator []
+
+//    contains(value)
+//    iterator find(value)
+
+//    remove_at(index)
+//    remove(value)
+//    remove_at(iterator)
+//    remove_last
+//    remove_first
+
+//    get size
+
+//    end()
+//    begin() for range based for
+
+};
+
+
 #endif
