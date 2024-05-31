332f8a676dfb32b738daf4b738d6bcd99736c18c
Valerii Sydorov <valeriisydorov@gmail.com>
2024-05-31 13:49:27 +0300

    Declaration functions in the EList class

--- EList/EList.h
+++ EList/EList.h
@@ -7,23 +7,17 @@
 
 template <typename T> class EList {
     class Node;
+    using pointer = Node*;
+public:
+    template <typename U, typename P> class Iterator;
 
     using value_type = T;
+    using difference_type = std::ptrdiff_t;
     using size_type = std::size_t;
-    using pointer_type = Node*;
+    using iterator = Iterator<T, pointer>;
+    using const_iterator = Iterator<const T, const pointer>;
 
-
-//    difference_type
-//    reference
-//    const_reference
-
-//    iterator
-//    const_iterator
-//    reverse_iterator
-//    const_reverse_iterator
-
-public:
-    class Iterator {
+    template <typename U, typename P> class Iterator {
     public:
     private:
 
@@ -37,41 +31,41 @@ public:
 
     ~EList();
 
+    iterator operator[](size_type);
+    const_iterator operator[](size_type) const;
+
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
+
     size_type size() const noexcept;
 
+    iterator insert(const_iterator, const value_type&);
     void push_back(const EList&);
     void push_front(const EList&);
     void pop_back();
     void pop_front();
 
     void remove(const EList&);
+    void remove(size_type);
+    void remove(iterator);
+
+    bool contains(const value_type&) const;
+    iterator find(const value_type&);
 
 private:
     class Node {
     public:
     private:
-        pointer_type prev;
-        pointer_type next;
+        pointer prev;
+        pointer next;
         value_type data;
     };
 
-    pointer_type head;
-    pointer_type tail;
+    pointer head;
+    pointer tail;
     size_type length;
-
-//    insert(index, value)
-
-//    iterator [] const
-//    iterator []
-
-//    contains(value)
-//    iterator find(value)
-
-//    remove_at(index)
-//    remove_at(iterator)
-
-//    end()
-//    begin() for range based for
 
 };
 
