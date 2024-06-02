092c4a0d4ad9c9d65f2063e3217bc30861622f89
Valerii Sydorov <valeriisydorov@gmail.com>
2024-06-02 12:43:49 +0300

    Declaration functions in the Iterator class

--- EList/EList.h
+++ EList/EList.h
@@ -14,15 +14,35 @@ public:
     template <typename U, typename P> class Iterator;
 
     using value_type = T;
-    using difference_type = std::ptrdiff_t;
     using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
     using iterator = Iterator<value_type, pointer>;
     using const_iterator = Iterator<const value_type, const pointer>;
 
     template <typename U, typename P> class Iterator {
     public:
-    private:
+        using value_type = U;
+        using pointer = P;
+        using reference = value_type&;
+        using const_reference = const value_type&;
 
+        Iterator(pointer node = nullptr);
+
+        reference operator*();
+        pointer operator->();
+
+        Iterator& operator++();
+        Iterator operator++(int);
+        Iterator operator+(difference_type) const;
+        Iterator& operator--();
+        Iterator operator--(int);
+        Iterator operator-(difference_type) const;
+
+        bool operator==(const Iterator&) const;
+        bool operator!=(const Iterator&) const;
+
+    private:
+        pointer current;
     };
 
     EList(size_type, const value_type&);
@@ -44,12 +64,12 @@ public:
     size_type size() const noexcept;
 
     iterator insert(const_iterator, const value_type&);
-    void push_back(const EList&);
-    void push_front(const EList&);
+    void push_back(const value_type&);
+    void push_front(const value_type&);
     void pop_back();
     void pop_front();
 
-    void remove(const EList&);
+    void remove(const value_type&);
     void remove(size_type);
     void remove(iterator);
 
