326f3a659973e0c8cad41c5f5144f4c97af873af
Valerii Sydorov <valeriisydorov@gmail.com>
2024-07-20 13:59:41 +0300

    Create common structure of the ESet class

--- ESet/ESet.h
+++ ESet/ESet.h
@@ -1,7 +1,48 @@
 #pragma once
 
 
-template <typename T>
+template <typename K>
 class ESet {
+
+public:
+
+    class Iterator {
+
+    public:
+
+    private:
+
+    };
+
+    ESet();
+    ESet(const ESet& other);
+    ESet(ESet&& other) noexcept;
+    ESet& operator=(const ESet& rhs);
+    ESet& operator=(ESet&& rhs) noexcept;
+    ~ESet();
+
+    // iterator begin();
+    // iterator end();
+
+    bool empty() const;
+    // size_type size();
+
+    void clear();
+    // bool insert(const key_type& key);
+    // size_type remove(const key_type& key);
+    // iterator remove_at(iterator pos);
+
+    // iterator find(const key_type& key);
+    // bool contains(const key_type& key) const;
+
+private:
+
+    class Node {
+
+    public:
+
+    private:
+
+    };
 
 };
