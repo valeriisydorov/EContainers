2856a6242ece6aeabb43df0473b82bea6954deee
Valerii Sydorov <valeriisydorov@gmail.com>
2024-08-03 12:14:27 +0300

    Declare some functions

--- EUnorderedMap/EUnorderedMap.h
+++ EUnorderedMap/EUnorderedMap.h
@@ -2,28 +2,45 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <functional>
 
 
+template<typename K, typename V, typename H = std::hash<K>>
+class EUnorderedMap
+{
+public:
+    class Iterator;
 
-// c-tor
-// d-tor
+    using key_type = K;
+    using size_type = std::size_t;
+    using iterator = Iterator;
 
-// copy c-tor
-// copy =
-// move c-tor
-// move =
+    EUnorderedMap();
+    EUnorderedMap(const EUnorderedMap& other);
+    EUnorderedMap(EUnorderedMap&& other) noexcept;
+    EUnorderedMap& operator=(const EUnorderedMap& rhs);
+    EUnorderedMap& operator=(EUnorderedMap&& rhs) noexcept;
 
-// getSize()
-// clear()
+    ~EUnorderedMap();
 
-// iterators begin() and end()
+    iterator begin() noexcept;
+    iterator begin() const noexcept;
+    iterator end() noexcept;
+    iterator end() const noexcept;
+
+    size_type size() const;
+
+    void clear();
 
 // insert(Key, Value)
-
-// Value* find(const Key&) - may return nullptr
-// Value& operator[](const Key&) - adds new element if not present. Investigate possible issues with this approach.
-
-// findByValue - find key by value
-
 // remove by key
 // remove by value
+
+// Value& operator[](const Key&) - adds new element if not present. Investigate possible issues with this approach.
+// Value* find(const Key&) - may return nullptr
+// findByValue - find key by value
+
+private:
+
+};
+
